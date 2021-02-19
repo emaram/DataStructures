bf4a9df4d54dfbf2287be8d203e108699ffec0e3
Eugen Marin <eugen.marin@outlook.com>
2021-02-19 16:42:27 +0200

    Modified man.cpp to run __TEST__

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
+#define __TEST__
+
 int main() {
 	SingleLinkedList list;
+#ifdef __TEST__
+	int val = 0;
+	for (int i = 0; i < 30; i++) {
+		val = rand() % 45;
+		if (val % 3 == 0)
+			list.addFirst(val);
+		else if (val % 3 == 1)
+			list.addLast(val);
+		else
+			list.insertNode(2, val);
+		list.printList();
+	}
+#else
 	int val = 0;
 	while (val >= 0) {
 		cout << "Input val (0 or negative for exit): ";
@@ -18,12 +33,15 @@ int main() {
 	cin >> val;
 	list.insertNode(3, val);
 	list.printList();
+#endif
+
 	cout << "Node at pos 3 is " << list.getValue(3) << endl;
  	cout << "Deleting 2 nodes ..." << endl;
 	list.deleteFirstNode();
 	list.printList();
 	list.deleteLastNode();
 	list.printList();
+
 	cout << "Clearing the list ..." << endl;
 	list.clearList();
 	list.printList();
